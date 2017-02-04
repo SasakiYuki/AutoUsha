792aa1dcee636f87b6256cdfedf1db34a3cd56bb
SasakiYuki <wacorggen@gmail.com>
2017-02-04 22:14:56 +0900

    first commit

---
+++ AutoUsha/app/src/main/java/com/example/yukin/autousha/model/BaseGrouwthObject.kt
@@ -0,0 +1,17 @@
+package com.example.yukin.autousha.model
+
+/**
+ * Created by yuki.n on 2017/02/04.
+ */
+data class BaseGrouwthObject (
+        val name:String,
+        val level:Int,
+        val exp:Int,
+        val hp:Int,
+        val mp:Int,
+        val attack:Int,
+        val defense:Int,
+        val magicAttack:Int,
+        val magicDefense:Int,
+        val speed:Int
+)
